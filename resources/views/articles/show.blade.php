d3ab0d3a4f29f9141475800c3d9bb4bcf365bb81
Noorarjun Sidhu <nsidhu73@my.bcit.ca>
2023-03-04 20:41:49 -0800

    Able to display an article's basic information when clicking the show button in the table.

---
+++ resources/views/articles/show.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('title', 'Show Article')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Show Article</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('articles.index') }}">Back</a>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Newsletter:</strong>
+            {{ $article->newsletter->Title }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Title:</strong>
+            {{ $article->Title }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Description:</strong>
+            {{ $article->Description }}
+        </div>
+    </div>
+</div>
+@endsection
