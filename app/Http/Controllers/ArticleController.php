0faf939824c7c97450069e180d2fa37a778b9aa6
Nhi Nguyen <nguyen.nhi5403@gmail.com>
2023-04-05 10:35:34 -0700

    Convert changes in ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -75,30 +75,6 @@ class ArticleController extends Controller
         return redirect()->route('articles.index')->with('success', 'Article updated successfully.');
     }
 
-    public function destroyConfirm($id) {
-        // Display the alert box
-        echo '<script>alert("Welcome to Geeks for Geeks")
-        if (confirm(`Are you sure you want to delete article $id?`)) {
-            // User confirmed, delete the article
-            fetch("/articles/' . $id . '", { method: "DELETE" })
-                .then(response => {
-                    if (response.ok) {
-                        // Article was deleted successfully, redirect to index
-                        window.location.href = "' . route('articles.index') . '";
-                    } else {
-                        // There was an error deleting the article, show an alert
-                        alert("Error deleting article");
-                    }
-                })
-                .catch(error => {
-                    // There was a network error, show an alert
-                    alert("Network error deleting article");
-                });
-        } else {
-            // User cancelled, return to the view
-            window.history.back();
-        }</script>';
-    }
     public function destroy($id): RedirectResponse
     {
         $article = Article::find($id); // find article by id(article id)
@@ -108,5 +84,4 @@ class ArticleController extends Controller
         return redirect() -> route('articles.index')
         -> with('success','Articles was deleted successfully');
     }
-
 }
